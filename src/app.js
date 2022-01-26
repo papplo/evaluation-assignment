b0cd88cfc2e12caad9e51c4a2e0f3d95c9b92ae8
Pablo Anttila <pablo@myrspoven.se>
2022-01-26 14:42:56 +0100

    App: put some styling and create logic to handle user input data as an react controlled component. Put some validation in fields. Allow for submit when form is valid.

--- src/app.js
+++ src/app.js
@@ -1,61 +1,134 @@
-import { useState } from 'react';
+import { useState, createRef } from 'react';
 
-const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const months = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12];
 const years = [2022, 2023, 2024, 2025, 2026, 2027]
 
 export function App() {
-    const [cardNumber, setCardNumber] = useState(undefined)
+    const [cardNumber, setCardNumber] = useState('')
     const [cardCVC, setCardCVC] = useState('')
     const [cardHolder, setCardHolder] = useState('');
-    const [selectedMonth, setSelectedMonth] = useState(undefined)
-    const [selectedYear, setSelectedYear] = useState(undefined)
+    const [selectedMonth, setSelectedMonth] = useState()
+    const [selectedYear, setSelectedYear] = useState()
 
-    function handleCardNumberChange(userCardNumberInput) {
-        setCardNumber(event.target.value);
+    const [validity, setValidity] = useState({
+        number: undefined,
+        holder: undefined,
+        month: undefined,
+        year: undefined,
+        cvc: undefined
+    })
+
+    function sanitizeStringToDigits(string) {
+       return string.replace(/[^0-9]/g, '')
     }
 
-    function handleCardHolderChange(userCardNameInput) {
+    function handleCardNumberChange(event) {
+        if (event.target.value.length < 19) {
+            setCardNumber(sanitizeStringToDigits(event.target.value))
+        }
+
+        const sanitizeUserInput = event.target.value.match(/[0-9]{4}/g)
+        if (sanitizeUserInput?.length == 4) {
+            setValidity(prev => ({
+                ...prev,
+                number: false
+            }))
+            setCardNumber(sanitizeUserInput.join(' '))
+        } else {
+            setValidity(prev => ({
+                ...prev,
+                number: true
+            }))
+        }
+
+    }
+
+    function handleCardHolderChange(event) {
         setCardHolder(event.target.value)
+        setValidity(prev => ({
+            ...prev,
+            holder: false
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log('thanks for submitting')
     }
 
     return (
         <main className="container">
             <section className="credit-card-form">
-                <form>
+                <article className="composition-credit-card">
+                    <div className="composition-emblem"></div>
+                    <div className="composition-card-number">{cardNumber}</div>
+                    <div className="composition-details">
+                        <div className="details-card-holder-name">
+                            Holder Name
+                            <h5>{cardHolder}&nbsp;</h5>
+                        </div>
+                        <div className="details-card-expiration-date">
+                            Expiration Date
+                            <h5>{selectedMonth?.toString().padStart(2,0)} / {selectedYear}</h5>
+                        </div>
+                    </div>
+                </article>
+                <form onSubmit={handleSubmit} >
                     <label htmlFor="cardNumber">Card Number</label>
-                    <input type="tel" name="cardNumber" inputmode="numeric" pattern="[0-9\s]{13,19}" autocomplete="cc-number" maxlength="19" placeholder="XXXX XXXX XXXX XXXX " defaultValue={cardNumber} onChange={handleCardNumberChange}/>
+                    <input
+                        type="text"
+                        name="cardNumber"
+                        contentEditable="false"
+                        required
+                        inputMode="numeric"
+                        pattern="[0-9\s]{13,19}"
+                        maxLength="19"
+                        placeholder="XXXX XXXX XXXX XXXX "
+                        value={cardNumber}
+                        onChange={handleCardNumberChange}
+                        aria-invalid={validity.number}
+                    />
 
                     <label htmlFor="cardName">Card Name</label>
-                    <input type="text" name="cardName" placeholder="Card Name" aria-label="Card Name" required defaultValue={cardHolder} onChange={handleCardHolderChange}/>
+                    <input 
+                        type="text" 
+                        name="cardName" 
+                        required 
+                        placeholder="Card Name" 
+                        aria-label="Card Name" 
+                        defaultValue={cardHolder} 
+                        onChange={handleCardHolderChange} 
+                        aria-invalid={validity.holder} 
+                    />
 
                     <div className="grid">
                         <fieldset id="date-selector">
-                            <label for="month">Expiration Month</label>
-                            <select id="month" name="month" required onSelect={(event) => setSelectedMonth(event.target.value)}>
+                            <label htmlFor="month">Expiration Month</label>
+                            <select id="month" name="month" required onChange={(event) => setSelectedMonth(event.target.value)}>
                                 <option value=''>Month</option>
                                 {months.map(month => (
-                                    <option value={month}>{month.toString().padStart(2, 0)}</option>
+                                    <option key={month} value={month}>{month.toString().padStart(2, 0)}</option>
                                 ))}
                             </select>
                         </fieldset>
                         <fieldset>
-                            <label for="month" visuallyHidden>Expiration Year</label>
-                            <select id="month" name="month" required onSelect={(event) => setSelectedYear(event.target.value)} >
+                            <label htmlFor="month">Expiration Year</label>
+                            <select id="year" name="year" required onChange={(event) => setSelectedYear(event.target.value)} >
                                 <option value=''>Year</option>
                                 {years.map(year => (
-                                    <option value={year}>{year}</option>
+                                    <option key={year} value={year}>{year}</option>
                                 ))}
                             </select>
                         </fieldset>
                         <fieldset>
-                            <label for="frmCCCVC">CVC</label>
-                            <input name="cvc" id="frmCCCVC" required autocomplete="cc-csc" maxLength="3" placeholder="* * *" defaultValue={cardCVC} onChange={(event) => SetCardCVC(event.target.value)}/>
+                            <label htmlFor="frmCCCVC">CVC</label>
+                            <input name="cvc" id="frmCCCVC" required inputtype="numeric" maxLength="3" placeholder="* * *" value={cardCVC} onChange={(event) => setCardCVC(event.target.value)} />
                         </fieldset>
                     </div>
 
                     <fieldset>
                         <label htmlFor="terms">
-                            <input type="checkbox" role="switch" id="terms" name="terms" />
+                            <input type="checkbox" role="switch" id="terms" name="terms" required/>
                             I agree to the <a href="#">Privacy Policy</a>
                         </label>
                     </fieldset>
