6e8232c5317aa9bb0a78ee45caf618ef5a2a5060
Pablo Anttila <pablo.anttila@gmail.com>
2022-01-27 09:17:13 +0100

    update index.js for deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDom from 'react-dom';
 import '@picocss/pico'
 import './app.css';
 
-import { App } from './App';
+import { App } from './app';
 
 const app = document.getElementById('app');
 ReactDom.render(<App />, app);
